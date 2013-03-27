e3dc673468d36e806c395f9aee03cc659529fa9c
Administrador <Administrador@192.168.153.128>
2013-03-27 05:06:42 +0100

    #2.-    Añadido el metodo deleteNULL, para detectar y borrar las conexiones perdidas o nulas. [en construcción] 		Falta encontrar la forma de saber el estado de un socket cliente desde el server.

--- src/serverbasic/LinkedListSocket.java
+++ src/serverbasic/LinkedListSocket.java
@@ -3,9 +3,10 @@
  * and open the template in the editor.
  */
 package serverbasic;
+import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,11 +16,12 @@ import java.util.List;
  * Añadiendo o borrando dichas conexioens.
  * 
  * @author Enrique Carvajal                 Creada el: 19/03/2013
- *                              Ultima modificacion:   20/03/2013
+ *                              Ultima modificacion:   27/03/2013
  */
 public class LinkedListSocket {
 
     public LinkedList<Socket> linkedListSocket;
+    private int index;
 //    public List listSocket;
     /**
      *
@@ -50,6 +52,48 @@ public class LinkedListSocket {
       while (it.hasNext()){
            System.out.println(it.next()+"");
        }        
+    }
+    
+    /*
+     * Busca socket null y los borra.
+     */
+    public void deleteNULL() throws SocketException, IOException{
+        System.out.println("Buscando socket Null");
+        Socket tem;
+        List lista2 = new ArrayList(linkedListSocket);
+        Iterator it = lista2.iterator();
+        index=0; // 1?
+       while (it.hasNext()){
+           System.out.println("parametros");
+           tem=linkedListSocket.get(index);
+           System.out.println(tem.isClosed());
+           System.out.println(tem.getKeepAlive());
+           System.out.println(tem.getOOBInline());
+           System.out.println(tem.getSoLinger());
+           System.out.println(tem.getChannel());
+           System.out.println(tem.getInputStream());
+           System.out.println(tem.getOutputStream());
+           System.out.println(tem.getRemoteSocketAddress());
+           System.out.println(tem.getReuseAddress());
+           System.out.println(tem.getSoTimeout());
+           System.out.println(tem.getTcpNoDelay());
+           System.out.println(tem.getTrafficClass());
+           System.out.println(tem.isBound());
+           System.out.println(tem.isConnected());
+           System.out.println(tem.isInputShutdown());
+           System.out.println(tem.isOutputShutdown());
+           
+           
+           if( tem.isClosed() ){
+               System.out.println("null");
+               
+               linkedListSocket.remove(index);
+           }
+           it.next();
+           index++;
+       }
+       listSocket();
+        
     }
     public List<Socket> getListSocket(){
         return new LinkedList<>(linkedListSocket);
